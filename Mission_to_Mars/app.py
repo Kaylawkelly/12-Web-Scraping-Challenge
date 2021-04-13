460304171ef97fe7f96948e356b820728166d2b3
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-04-13 14:30:35 -0400

    commit

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -2,23 +2,37 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-
+# Create an instance of Flask
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+
+# Route to render index.html template using data from Mongo
 @app.route("/")
-def index():
-    mars_data = mongo.db.mars.find_one()
-    # print(mars)
-    return render_template("index.html", data=mars_data)
+def home():
 
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars=mars_data)
+
+
+# Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():  
-    mars_scrape = scrape_mars.scrape()
-    print(mars_scrape)
-    mongo.db.mars.update({}, mars_scrape, upsert=True)
-    return redirect("http://localhost:5000/", code=302)
+def scrape():
+
+    # Run the scrape function
+    mars = scrape_mars()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,210 +1,83 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#pip install nbconvert
-
-
-# In[2]:
-
-
-# Dependencies and Setup
-from bs4 import BeautifulSoup
-from splinter import Browser
-import pandas as pd
-import time
-from webdriver_manager.chrome import ChromeDriverManager
-import nbconvert
-
-
-# In[3]:
-
-
-# MAC: Set Executable Path & Initialize Chrome Browser
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# In[4]:
-
-
-mars={}
-
-
-# In[5]:
-
-
-#NASA Mars News Site
-url = "https://redplanetscience.com/"
-browser.visit(url)
-
-
-# In[6]:
-
-
-# Parse Results HTML with BeautifulSoup>
-
-html = browser.html
-news_soup = BeautifulSoup(html, "html.parser")
-
-
-# In[7]:
-
-
-#News
-news_title=news_soup.find("div", class_="content_title").text
-
-
-# In[8]:
-
-
-news_p=news_soup.find("div", class_="article_teaser_body").text
-
-
-# In[9]:
-
-
-mars["news_titles"]=news_title
-mars["news_p"]=news_p
-
-
-# In[10]:
-
-
-#JPL Mars Space Image
-url = "https://spaceimages-mars.com/"
-browser.visit(url)
-browser.find_by_tag("button")[1].click()
-
-
-# In[11]:
-
-
-html = browser.html
-jplimage = BeautifulSoup(html, "html.parser")
-
-
-# In[12]:
-
-
-image=jplimage.find('img',class_="fancybox-image").get('src')
-
-
-# In[13]:
-
-
-image
-
-
-# In[14]:
-
-
-featimgurl="https://spaceimages-mars.com/"+image
-
-
-# In[15]:
-
-
-featimgurl
-
-
-# In[16]:
-
-
-mars["featured_image_url"]=featimgurl
-
-
-# In[17]:
-
-
-#Facts
-url = "https://galaxyfacts-mars.com/"
-df=pd.read_html(url)
-df=df[0]
-df
-
-
-# In[18]:
-
-
-df.columns=["Description","Mars","Earth"]
-df.set_index("Description",inplace=True)
-df
-
-
-# In[19]:
-
-
-df_html=df.to_html()
-df_html
-
-
-# In[20]:
-
-
-mars["facts"]=df_html
-
-
-# In[21]:
-
-
-#hemispheres
-url = "https://marshemispheres.com/"
-browser.visit(url)
-result=browser.find_by_css("a.product-item img")
-
-hemisphere_image_url=[]
-
-for i in range(len(result)):
-    hemisphere={}
-    browser.find_by_css("a.product-item img")[i].click()
-    element = browser.links.find_by_text('Sample').first
-    img_url= element["href"]
-    hemisphere["img_url"]=img_url
-    hemisphere["title"]=browser.find_by_css("h2.title").text
-    hemisphere_image_url.append(hemisphere)
-    browser.back()
-
-
-# In[22]:
-
-
-hemisphere_image_url
-
-
-# In[23]:
-
-
-mars["hemispheres"]=hemisphere_image_url
-
-
-# In[24]:
-browser.quit()
-
-
-
-
-
-# In[25]:
-
-
-#convert to notebook to python script
-
-#delete some lines (cells, ie run blah)
-#grab to app.py (Day 3)
-#index.html use same variables
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+def scrape():
+    # Import dependencies
+    from splinter import Browser
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
+    import pandas as pd
+  
+ # MAC: Set Executable Path & Initialize Chrome Browser
+   
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+   
+    mars={}
+
+    #-----------NASA MARS NEWS---------------
+    url = "https://redplanetscience.com/"
+    browser.visit(url)
+    # Parse Results HTML with BeautifulSoup>
+    
+    html = browser.html
+    news_soup = BeautifulSoup(html, "html.parser")
+    #News
+    news_title=news_soup.find("div", class_="content_title").text
+    news_p=news_soup.find("div", class_="article_teaser_body").text
+    mars["news_titles"]=news_title
+    mars["news_p"]=news_p
+
+    #-------------JPL MARS SPACE IMAGES - FEATURED IMAGE-------------
+    url = "https://spaceimages-mars.com/"
+    browser.visit(url)
+    browser.find_by_tag("button")[1].click()
+    html = browser.html
+    jplimage = BeautifulSoup(html, "html.parser")
+    image=jplimage.find('img',class_="fancybox-image").get('src')
+    image
+    featimgurl="https://spaceimages-mars.com/"+image
+    featimgurl
+    mars["featured_image_url"]=featimgurl
+
+
+    #-------------- MARS FACTS---------------
+    url = "https://galaxyfacts-mars.com/"
+    df=pd.read_html(url)
+    df=df[0]
+    df
+    df.columns=["Description","Mars","Earth"]
+    df.set_index("Description",inplace=True)
+    df
+    df_html=df.to_html()
+    df_html
+    mars["facts"]=df_html
+
+    #----------- MARS HEMISPHERES---------------
+    url = "https://marshemispheres.com/"
+    browser.visit(url)
+    result=browser.find_by_css("a.product-item img")
+    
+    hemisphere_image_url=[]
+    
+    for i in range(len(result)):
+        hemisphere={}
+        browser.find_by_css("a.product-item img")[i].click()
+        element = browser.links.find_by_text('Sample').first
+        img_url= element["href"]
+        hemisphere["img_url"]=img_url
+        hemisphere["title"]=browser.find_by_css("h2.title").text
+        hemisphere_image_url.append(hemisphere)
+        browser.back()
+    hemisphere_image_url
+    mars["hemispheres"]=hemisphere_image_url
+
+    # Return one python dictionary containing all of the scraped data
+    mars = {'news_title':news_title}
+    mars ['news_p']=news_p
+    mars ['featured_image_url']=featimgurl
+    mars['facts']=df_html
+    mars['hemispheres']=hemisphere_image_url
+
+    # close the browser
+    browser.quit()
+
+    return mars
